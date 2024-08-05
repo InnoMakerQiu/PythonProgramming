bc1b0dc357955fe4b18a3bb5ee2bbe66d4f9fbfb
2377767409 <2377767409@qq.com>
2024-08-05 11:25:33 +0800

    update enumeration.py

--- Work/enumeration_method.py
+++ Work/enumeration_method.py
@@ -13,31 +13,54 @@
 
 import math
 
-# 枚举的范围为根号N/2<n<=根号N
-# 采用深度优先搜索的思想
-# 使用一个列表进行记录a,b,c,d的值，比如d=res_list[0]..a=res_list[3]
-# dfs函数中需要携带depth深度信息，res_num（for in range(depth): src_num-res_list[i])
-# 首先将x4的取值为
-res_lists = []
-my_list= [0,0,0,0]
+# # 枚举的范围为根号N/2<n<=根号N
+# # 采用深度优先搜索的思想
+# # 使用一个列表进行记录a,b,c,d的值，比如d=res_list[0]..a=res_list[3]
+# # dfs函数中需要携带depth深度信息，res_num（for in range(depth): src_num-res_list[i])
+# # 首先将x4的取值为
+# res_lists = []
+# my_list= [0,0,0,0]
 
-def dfs(depth,res_num):
-    if depth==3:
-        my_list[depth] = int(math.sqrt(res_num))
-        res_num = res_num - my_list[depth]**2
-        if res_num==0:
-            res_lists.append(list(reversed(my_list)))
-        return
-    max_range = int(math.sqrt(res_num))
-    min_range = int(math.sqrt(res_num/(4-depth)))
-    for i in range(max_range,min_range-1,-1):
-        # print(f"the depth : {depth}, the res_num : {res_num}")
-        # print(f"res_num: {res_num}, max_range: {max_range}, min_range: {min_range}, i: {i}")
-        my_list[depth] = i
-        num = res_num - i**2
-        dfs(depth+1,num)
+# def dfs(depth,res_num):
+#     if depth==3:
+#         my_list[depth] = int(math.sqrt(res_num))
+#         res_num = res_num - my_list[depth]**2
+#         if res_num==0:
+#             res_lists.append(list(reversed(my_list)))
+#         return
+#     max_range = int(math.sqrt(res_num))
+#     min_range = int(math.sqrt(res_num/(4-depth)))
+#     for i in range(max_range,min_range-1,-1):
+#         # print(f"the depth : {depth}, the res_num : {res_num}")
+#         # print(f"res_num: {res_num}, max_range: {max_range}, min_range: {min_range}, i: {i}")
+#         my_list[depth] = i
+#         num = res_num - i**2
+#         dfs(depth+1,num)
 
-des_num = int(input())
-dfs(0,des_num)
-res_lists.sort()
-print(*res_lists[0], sep=' ')
+# des_num = int(input())
+# dfs(0,des_num)
+# res_lists.sort()
+# print(*res_lists[0], sep=' ')
+
+
+
+import os
+import sys
+from math import sqrt
+ 
+# print(sys.version)
+# 下面是来源于https://lrl52.top/1146/python-lanqiaocup/
+n = int(input())
+l1 = int(sqrt(n))
+for i in range(0, l1 + 1):
+    l2 = int(sqrt(n - i * i))
+    if l2 < i: break
+    for j in range(i, l2 + 1):
+        l3 = int(sqrt(n - i * i - j * j))
+        if l3 < j: break
+        for k in range(j, l3 + 1):
+            t = int(sqrt(n - i * i - j * j - k * k))
+            if t < k: break
+            if i * i + j * j + k * k + t * t == n:
+                print(i, j, k, t)
+                sys.exit(0)
